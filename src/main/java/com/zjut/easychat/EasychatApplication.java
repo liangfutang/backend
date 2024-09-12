abf24574c1c6b0d4bb78d8bd074fbf805667e887
liangfutang <tanglf@kalman-navigation.com>
2024-09-12 15:52:20 +0800

    1. 初始化springboot项目

---
+++ src/main/java/com/zjut/easychat/EasychatApplication.java
@@ -0,0 +1,15 @@
+package com.zjut.easychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tlf
+ */
+@SpringBootApplication
+public class EasychatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasychatApplication.class, args);
+    }
+}
