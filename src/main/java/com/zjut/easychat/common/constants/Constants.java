af371a4378a2a495921d10e53d6e2f3673765364
liangfutang <tanglf@kalman-navigation.com>
2024-09-12 16:16:06 +0800

    1. 添加项目基础模块

---
+++ src/main/java/com/zjut/easychat/common/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.zjut.easychat.common.constants;
+
+/**
+ * 常用的常量
+ */
+public interface Constants {
+
+}
---
+++ src/main/java/com/zjut/easychat/common/exception/ParameterException.java
@@ -0,0 +1,51 @@
+package com.zjut.easychat.common.exception;
+
+import com.zjut.easychat.common.convention.code.ResultCodeEnum;
+
+/**
+ * 参数异常
+ * @author jack
+ */
+public class ParameterException extends RuntimeException {
+    private Integer code;
+
+    public ParameterException() {
+        super(ResultCodeEnum.PARAMETER_ILLEGAL.message());
+        this.code = ResultCodeEnum.PARAMETER_ILLEGAL.code();
+    }
+
+    public ParameterException(Integer code, String message) {
+        super(message);
+        this.code = ResultCodeEnum.PARAMETER_ILLEGAL.code();
+        this.code = code;
+    }
+
+    public ParameterException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.message());
+        this.code = ResultCodeEnum.PARAMETER_ILLEGAL.code();
+        this.code = resultCodeEnum.code();
+    }
+
+    public ParameterException(String message) {
+        super(message);
+        this.code = ResultCodeEnum.PARAMETER_ILLEGAL.code();
+    }
+
+    public ParameterException(String message, Throwable throwable) {
+        super(message, throwable);
+        this.code = ResultCodeEnum.PARAMETER_ILLEGAL.code();
+    }
+
+    public ParameterException(Throwable throwable) {
+        super(throwable);
+        this.code = ResultCodeEnum.PARAMETER_ILLEGAL.code();
+    }
+
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
